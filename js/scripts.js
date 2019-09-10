672e89fd43c06fbd751124cba93b351d2a2f2e77
Franklin Waggoner and Huntley McNabb <huntley@>
2019-09-09 20:34:19 -0700

    add a second madlib and add animations

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,26 @@
-$(document).ready(function() {
-  $("#blanks form").submit(function(event) {
-    var pluralNoun1Input = $("input#pluralNoun1").val();
-    var pluralNoun2Input = $("input#pluralNoun2").val();
-    var pluralNoun3Input = $("input#pluralNoun3").val();
-    var ingVerbInput = $("input#ingVerb").val();
-    var noun1Input = $("input#noun1").val();
-    var noun2Input = $("input#noun2").val();
-    var verb1Input = $("input#verb1").val();
-
-    $(".pluralNoun1").text(pluralNoun1Input);
-    $(".pluralNoun2").text(pluralNoun2Input);
-    $(".pluralNoun3").text(pluralNoun3Input);
-    $(".ingVerb").text(ingVerbInput);
-    $(".verb1").text(verb1Input);
-    $(".noun1").text(noun1Input);
-    $(".noun2").text(noun2Input);
-
-    $("#story").show();
-
-    event.preventDefault();
-  });
-});
+// $(document).ready(function() {
+//   $("#blanks form").submit(function(event) {
+//     var pluralNoun1Input = $("input#pluralNoun1").val();
+//     var pluralNoun2Input = $("input#pluralNoun2").val();
+//     var pluralNoun3Input = $("input#pluralNoun3").val();
+//     var ingVerbInput = $("input#ingVerb").val();
+//     var noun1Input = $("input#noun1").val();
+//     var noun2Input = $("input#noun2").val();
+//     var verb1Input = $("input#verb1").val();
+//
+//     $(".pluralNoun1").text(pluralNoun1Input);
+//     $(".pluralNoun2").text(pluralNoun2Input);
+//     $(".pluralNoun3").text(pluralNoun3Input);
+//     $(".ingVerb").text(ingVerbInput);
+//     $(".verb1").text(verb1Input);
+//     $(".noun1").text(noun1Input);
+//     $(".noun2").text(noun2Input);
+//
+//     $("#story").show();
+//
+//     event.preventDefault();
+//   });
+// });
 
 $(document).ready(function() {
   $("#blanks form").submit(function(event) {
@@ -38,7 +38,12 @@ $(document).ready(function() {
     $(".verb").text(verbInput);
     $(".noun").text(nounInput);
 
-    $("#story").show();
+    $("#story").show()
+    $("#blanks").hide();
+    $("#hideAfter").hide();
+    $("#story").animate({width: '100px'}, "slow").animate({width: '1000px'}, "slow").animate({width: '100px'}, "slow").animate({width: '1000px'}, "slow").animate({width: '100px'}, "slow").animate({width: '1000px'}, "slow").animate({width: '100px'}, "slow").animate({width: '1000px'}, "slow").animate({width: '100px'}, "slow").animate({width: '1000px'}, "slow")
+
+
 
     event.preventDefault();
   });
